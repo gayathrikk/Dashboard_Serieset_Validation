6605154909a22ae2478ff290860be68fae3bdc8e
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2026-04-24 15:46:12 +0530

    Update Dashboard_Serieset_Validation.java

--- src/test/java/TCases/Automation/Dashboard_Serieset_Validation.java
+++ src/test/java/TCases/Automation/Dashboard_Serieset_Validation.java
@@ -18,7 +18,7 @@ public class Dashboard_Serieset_Validation {
 	@BeforeTest
 	public void setup() throws MalformedURLException {
 		DesiredCapabilities dc = DesiredCapabilities.chrome();
-		URL url = new URL("http://172.22.0.5:4444/wd/hub");
+		URL url = new URL("http://172.29.0.4:4444/wd/hub");
 		driver = new RemoteWebDriver(url, dc);
 	}
 
